aed3d9bd8e07d879b579bd4c90dbefeadf8b428a
639dev <gharood_3@hotmail.com>
2019-01-24 13:42:48 +0300

    first commit

---
+++ src/components/MovieDetails.js
@@ -0,0 +1,16 @@
+import React from "react";
+import StyledHeader from "./StyledHeader";
+import FontAwesomeIcon from "./FontAwesomeIcon";
+import StyledHeaderLink from "./StyledHeaderLink";
+
+const MovieDetails = () => {
+  return (
+    <StyledHeader>
+      <StyledHeaderLink href="/">
+        <FontAwesomeIcon icon="chevron-left" text="Go back" />
+      </StyledHeaderLink>
+    </StyledHeader>
+  );
+};
+
+export default MovieDetails;
