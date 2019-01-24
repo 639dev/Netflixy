81df9165d50319ad4f9fe87edc05cf27ccc96fd0
639dev <gharood_3@hotmail.com>
2019-01-24 14:59:49 +0300

    setting up middleware for fetching data from an api

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, Component } from "react";
 import FontAwesomeIcon from "./FontAwesomeIcon";
 import StyledHeader from "./StyledHeader";
 import StyledHeaderTitle from "./StyledHeaderTitle";
@@ -7,31 +7,37 @@ import StyledHorizontalScroll from "./StyledHorizontalScroll";
 import Movie from "./Movie";
 import StyledFooter from "./StyledFooter";
 import StyledLargeBtn from "./StyledLargeBtn";
-const Movies = () => {
-  return (
-    <Fragment>
-      <StyledHeader>
-        <FontAwesomeIcon icon="bars" text="help" />
-        <StyledHeaderTitle>The Movie Recommender</StyledHeaderTitle>
-        <FontAwesomeIcon icon="search" />
-      </StyledHeader>
-      {/* the list of movies */}
-      <StyledHorizontalScroll>
-        {movies.map(movie => (
-          <Movie
-            key={movie.id}
-            name={movie.name}
-            poster={movie.poster}
-            duration={movie.duration}
-            year={movie.year}
-          />
-        ))}
-      </StyledHorizontalScroll>
-      <StyledFooter>
-        <StyledLargeBtn>Get Recommended Movies</StyledLargeBtn>
-      </StyledFooter>
-    </Fragment>
-  );
-};
+
+class Movies extends Component {
+  componentDidMount() {
+    this.props.getMovies();
+  }
+  render() {
+    return (
+      <Fragment>
+        <StyledHeader>
+          <FontAwesomeIcon icon="bars" text="help" />
+          <StyledHeaderTitle>The Movie Recommender</StyledHeaderTitle>
+          <FontAwesomeIcon icon="search" />
+        </StyledHeader>
+        {/* the list of movies */}
+        <StyledHorizontalScroll>
+          {this.props.movies.map(movie => (
+            <Movie
+              key={movie.id}
+              name={movie.name}
+              poster={movie.poster}
+              duration={movie.duration}
+              year={movie.year}
+            />
+          ))}
+        </StyledHorizontalScroll>
+        <StyledFooter>
+          <StyledLargeBtn>Get Recommended Movies</StyledLargeBtn>
+        </StyledFooter>
+      </Fragment>
+    );
+  }
+}
 
 export default Movies;
